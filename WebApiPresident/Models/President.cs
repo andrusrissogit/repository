79b006e72283bf207b67f5bd788e42727decb069
andrea.risso <andrea.risso@AR-IT02590>
2018-08-09 20:41:11 -0300

    WebApiPresident

---
+++ WebApiPresident/Models/President.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiPresident.Models
+{
+    public class President
+    {
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Birthplace { get; set; }
+        public DateTime? Deathday { get; set; }
+        public string Deathplace { get; set; }
+    }
+}
